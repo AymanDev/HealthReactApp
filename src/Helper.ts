85e931376c09441fa4e8af8b18cd0d31ba3a4d79
AymanDev <maxsa21@gmail.com>
2019-06-23 14:53:40 +0300

    Statistics

--- src/Helper.ts
+++ src/Helper.ts
@@ -1,9 +1,11 @@
 import { SnilsData } from "./components/PanelPage/panel";
 
-export const URL_DOMAIN = "http://730b8391.ngrok.io/";
+export const URL_DOMAIN = "http://c32a7f10.ngrok.io/";
 export const AUTH = "api/Health/Auth";
 export const SNILS = "api/Health/GetClient";
 export const DISEASE = "api/Health/GetDiseaseHistory";
+export const DICTIONARIES = "api/Health/GetDictionaries";
+export const STATISTICS = "api/Health/GetStatisticByCityAndDate";
 
 export const EMPTY_SNILS_DATA: SnilsData = {
   allergy: [],
@@ -36,10 +38,27 @@ export interface SnilsRequest {
   snils: string;
 }
 
+export interface StatisticsRequest {
+  cityId: string;
+  diseaseId: string;
+  startDate: string;
+  endDate: string;
+}
+
 export async function postData(url, data): Promise<any> {
   const reponse = await fetch(url, {
     method: "POST",
     body: JSON.stringify(data),
+    headers: new Headers({
+      "Content-Type": "application/json"
+    })
+  });
+  return await reponse.json();
+}
+
+export async function getData(url): Promise<any> {
+  const reponse = await fetch(url, {
+    method: "GET",
     headers: new Headers({
       "Content-Type": "application/json"
     })
--- src/components/PanelPage/panel.tsx
+++ src/components/PanelPage/panel.tsx
@@ -23,12 +23,14 @@ import {
   SnilsRequest,
   EMPTY_SNILS_DATA
 } from "../../Helper";
+import StatisticsPage from "../StatisticPage/statistics";
 const logo = require("./images/logo.png");
 interface PanelPageState {
   snils?: string;
   loading?: boolean;
   snilsData?: SnilsData;
   createMode: boolean;
+  activeTab: string;
 }
 export interface SnilsData {
   allergy: string[];
@@ -64,7 +66,8 @@ class PanelPage extends React.Component<any, PanelPageState> {
     snils: "",
     snilsData: null,
     loading: false,
-    createMode: false
+    createMode: false,
+    activeTab: "user-data"
   };
 
   logout() {
@@ -97,7 +100,10 @@ class PanelPage extends React.Component<any, PanelPageState> {
       <div>
         <Navbar className="navbar justify-content-between">
           <Container>
-            <Navbar.Brand href="/panel" className="d-flex flex-row justify-content-center align-items-center">
+            <Navbar.Brand
+              href="/panel"
+              className="d-flex flex-row justify-content-center align-items-center"
+            >
               <img
                 src={logo}
                 alt="Logo"
@@ -107,53 +113,88 @@ class PanelPage extends React.Component<any, PanelPageState> {
               />
               <strong className="title ml-2 mt-3">Health</strong>
             </Navbar.Brand>
+            <Nav className="mr-auto mt-3">
+              <Nav.Link
+                className={
+                  "ml-4 menu-item" +
+                  (this.state.activeTab === "user-data" ? " active" : "")
+                }
+                onClick={() => this.setState({ activeTab: "user-data" })}
+              >
+                Пациенты
+              </Nav.Link>
+              <Nav.Link
+                className={
+                  "ml-4 menu-item" +
+                  (this.state.activeTab === "statistics" ? " active" : "")
+                }
+                onClick={() => this.setState({ activeTab: "statistics" })}
+              >
+                Статистика
+              </Nav.Link>
+            </Nav>
             <Button className="button" onClick={() => this.logout()}>
               Выйти
             </Button>
           </Container>
         </Navbar>
-        <Container className="snils-bar">
-          <Row>
-            <Col xs={3}>
-              <FormControl
-                className="input text-center"
-                placeholder="Поиск пациента по СНИЛС"
-                aria-label="Поиск пациента по СНИЛС"
-                onChange={event => this.setState({ snils: event.target.value })}
-              />
-            </Col>
-            <Col xs={1} className="p-0">
-              <Button className="button" onClick={() => this.search()}>
-                Найти
-              </Button>
-            </Col>
-            <Col xs={3} className="ml-4">
-              <Button
-                className="button outline"
-                onClick={() => this.enterCreateMode()}
-              >
-                Завести новую карточку
-              </Button>
-            </Col>
-          </Row>
-        </Container>
-        <Container>
-          {(() => {
-            if (this.state.snilsData !== null) {
-              return <UserData data={this.state.snilsData} />;
-            }
-            if (this.state.createMode) {
-              return <UserData data={EMPTY_SNILS_DATA} editable={true} />;
-            }
-            if (this.state.loading) {
-              return (
-                <Container className="w-100 h-100 d-flex align-items-center justify-content-center color-primary">
-                  <FontAwesomeIcon icon={faSpinner} spin size="5x" />
+        {(() => {
+          if (this.state.activeTab === "statistics") {
+            return <StatisticsPage />;
+          }
+          if (this.state.activeTab === "user-data") {
+            return (
+              <div>
+                <Container className="snils-bar">
+                  <Row>
+                    <Col xs={3}>
+                      <FormControl
+                        className="input text-center"
+                        placeholder="Поиск пациента по СНИЛС"
+                        aria-label="Поиск пациента по СНИЛС"
+                        onChange={event =>
+                          this.setState({ snils: event.target.value })
+                        }
+                      />
+                    </Col>
+                    <Col xs={1} className="p-0">
+                      <Button className="button" onClick={() => this.search()}>
+                        Найти
+                      </Button>
+                    </Col>
+                    <Col xs={3} className="ml-4">
+                      <Button
+                        className="button outline"
+                        onClick={() => this.enterCreateMode()}
+                      >
+                        Завести новую карточку
+                      </Button>
+                    </Col>
+                  </Row>
                 </Container>
-              );
-            }
-          })()}
-        </Container>
+                <Container>
+                  {(() => {
+                    if (this.state.snilsData !== null) {
+                      return <UserData data={this.state.snilsData} />;
+                    }
+                    if (this.state.createMode) {
+                      return (
+                        <UserData data={EMPTY_SNILS_DATA} editable={true} />
+                      );
+                    }
+                    if (this.state.loading) {
+                      return (
+                        <Container className="w-100 h-100 d-flex align-items-center justify-content-center color-primary">
+                          <FontAwesomeIcon icon={faSpinner} spin size="5x" />
+                        </Container>
+                      );
+                    }
+                  })()}
+                </Container>
+              </div>
+            );
+          }
+        })()}
       </div>
     );
   }
